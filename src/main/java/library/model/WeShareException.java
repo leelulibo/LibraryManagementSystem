a6b9097acc1b521c838861f78b8ddcfa14825ff4
Lulibo <leelulibo@gmail.com>
2024-06-11 11:07:17 +0200

    login admin

---
+++ src/main/java/library/model/WeShareException.java
@@ -0,0 +1,12 @@
+package library.model;
+
+/*
+ ** DO NOT CHANGE!!
+ */
+
+
+public class WeShareException extends RuntimeException {
+    public WeShareException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,29 @@
+// src/main/java/org/example/User.java
+package org.example;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
